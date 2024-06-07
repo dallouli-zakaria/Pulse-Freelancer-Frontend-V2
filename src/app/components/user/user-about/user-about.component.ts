41f4a5e0ff6ccff9898cd5edd729e958974dfd46
zakaria <dalloulizakaria@gmail.com>
2024-06-07 10:45:54 +0100

    second commit

---
+++ src/app/components/user/user-about/user-about.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-about',
+  templateUrl: './user-about.component.html',
+  styleUrl: './user-about.component.css'
+})
+export class UserAboutComponent {
+
+}
