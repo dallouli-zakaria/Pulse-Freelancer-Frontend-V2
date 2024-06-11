4eec4efe096f69869aa6e7a4e263b35b874cf565
zakaria <dalloulizakaria@gmail.com>
2024-06-11 09:46:58 +0100

    adding view freelancers page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminDashboardComponent } from './components/admin/admin-dashboard/admin-dashboard.component';
 import { AdminTopbarComponent } from './components/admin/admin-layout/admin-topbar/admin-topbar.component';
+import { UserIndexComponent } from './components/user/user-index/user-index.component';
+import { UserAboutComponent } from './components/user/user-about/user-about.component';
+import { UserHomeComponent } from './components/user/user-home/user-home.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { AuthentificationComponent } from './components/auth/authentification/authentification.component';
+import { UserViewFreelancersComponent } from './components/user/user-view-freelancers/user-view-freelancers.component';
 
 const routes: Routes = [
   {path:'admin',component:AdminDashboardComponent},
-  {path:'top',component:AdminTopbarComponent}
+  {path:'top',component:AdminTopbarComponent},
+  {path:'home',component:UserHomeComponent},
+  {path:'about',component:UserAboutComponent},
+  {path:'register',component:AuthentificationComponent},
+  {path:'login',component:LoginComponent},
+  {path:'view-freelancers',component:UserViewFreelancersComponent}
+  
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,33 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AdminDashboardComponent } from './components/admin/admin-dashboard/admin-dashboard.component';
 import { AdminLeftbarComponent } from './components/admin/admin-layout/admin-leftbar/admin-leftbar.component';
 import { AdminTopbarComponent } from './components/admin/admin-layout/admin-topbar/admin-topbar.component';
+import { UserIndexComponent } from './components/user/user-index/user-index.component';
+import { UserHeaderComponent } from './components/user/user-layout/user-header/user-header.component';
+import { UserFooterComponent } from './components/user/user-layout/user-footer/user-footer.component';
+import { UserAboutComponent } from './components/user/user-about/user-about.component';
+import { UserHomeComponent } from './components/user/user-home/user-home.component';
+import { UserContactComponent } from './components/user/user-contact/user-contact.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { AuthentificationComponent } from './components/auth/authentification/authentification.component';
+import { UserViewFreelancersComponent } from './components/user/user-view-freelancers/user-view-freelancers.component';
+import { UserProfileComponent } from './components/user/user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminDashboardComponent,
     AdminLeftbarComponent,
-    AdminTopbarComponent
+    AdminTopbarComponent,
+    UserIndexComponent,
+    UserHeaderComponent,
+    UserFooterComponent,
+    UserAboutComponent,
+    UserHomeComponent,
+    UserContactComponent,
+    LoginComponent,
+    AuthentificationComponent,
+    UserViewFreelancersComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
