c3846bf0595b355fea247afa7c682c95599ef0a7
zakaria <dalloulizakaria@gmail.com>
2024-06-12 13:53:28 +0100

    adding offers page

---
+++ src/app/components/freelancer/apply-offer/apply-offer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-apply-offer',
+  templateUrl: './apply-offer.component.html',
+  styleUrl: './apply-offer.component.css'
+})
+export class ApplyOfferComponent {
+
+}
---
+++ src/app/components/freelancer/view-offers/view-offers.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-offers',
+  templateUrl: './view-offers.component.html',
+  styleUrl: './view-offers.component.css'
+})
+export class ViewOffersComponent {
+
+}
