de4f92589563f18143e8a989d1e012c23abfcd19
zakaria <dalloulizakaria@gmail.com>
2024-06-13 15:24:22 +0100

    commit n 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -53,6 +53,28 @@ import { PermissionTableComponent } from './components/admin/table/permission-ta
 import { RoleTableComponent } from './components/admin/table/role-table/role-table.component';
 import { UsersTableComponent } from './components/admin/table/users-table/users-table.component';
 import { ViewOffersDetailsComponent } from './components/freelancer/view-offers-details/view-offers-details.component';
+import { AuthentificationComponent } from './components/auth/authentification/authentification.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { Error404Component } from './components/errors/error404/error404.component';
+import { Error500Component } from './components/errors/error500/error500.component';
+import { ApplyOfferComponent } from './components/freelancer/apply-offer/apply-offer.component';
+import { FreelancerProfileComponent } from './components/freelancer/freelancer-profile/freelancer-profile.component';
+import { ViewOffersComponent } from './components/freelancer/view-offers/view-offers.component';
+import { ContactUsComponent } from './components/user/contact-us/contact-us.component';
+import { UserAboutComponent } from './components/user/user-about/user-about.component';
+import { UserAddOfferComponent } from './components/user/user-add-offer/user-add-offer.component';
+import { UserContractComponent } from './components/user/user-contract/user-contract.component';
+import { UserDashboardComponent } from './components/user/user-dashboard/user-dashboard.component';
+import { UserHomeComponent } from './components/user/user-home/user-home.component';
+import { UserIndexComponent } from './components/user/user-index/user-index.component';
+import { UserFooterComponent } from './components/user/user-layout/user-footer/user-footer.component';
+import { UserHeaderComponent } from './components/user/user-layout/user-header/user-header.component';
+import { UserProfileComponent } from './components/user/user-profile/user-profile.component';
+import { UserTestComponent } from './components/user/user-test/user-test.component';
+import { UserUpdateOfferComponent } from './components/user/user-update-offer/user-update-offer.component';
+import { UserUpdateProfileComponent } from './components/user/user-update-profile/user-update-profile.component';
+import { UserViewFreelancersComponent } from './components/user/user-view-freelancers/user-view-freelancers.component';
+import { ViewClientOfferComponent } from './components/user/view-client-offer/view-client-offer.component';
 
 @NgModule({
   declarations: [
@@ -105,7 +127,35 @@ import { ViewOffersDetailsComponent } from './components/freelancer/view-offers-
           RoleAddComponent,
           PermissionAddComponent,
           RoleDetailsComponent,
-          ViewOffersDetailsComponent
+          ViewOffersDetailsComponent,
+          AppComponent,
+          AdminDashboardComponent,
+          AdminLeftbarComponent,
+          AdminTopbarComponent,
+          UserIndexComponent,
+          UserHeaderComponent,
+          UserFooterComponent,
+          UserAboutComponent,
+          UserHomeComponent,
+          LoginComponent,
+          AuthentificationComponent,
+          UserViewFreelancersComponent,
+          UserProfileComponent,
+          FreelancerProfileComponent,
+          UserAddOfferComponent,
+          UserDashboardComponent,
+        
+          Error404Component,
+          Error500Component,
+          UserContractComponent,
+          ViewOffersComponent,
+          ApplyOfferComponent,
+          ViewClientOfferComponent,
+          UserUpdateProfileComponent,
+          ContactUsComponent,
+          ViewOffersDetailsComponent,
+          UserUpdateOfferComponent,
+          UserTestComponent
   ],
   imports: [
       BrowserModule,
