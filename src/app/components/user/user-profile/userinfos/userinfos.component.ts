a758f9010cceceaebd6ba7c189658785ce9463d7
zakaria <dalloulizakaria@gmail.com>
2024-06-27 15:58:28 +0100

    preparing for jawad pull

--- src/app/components/user/user-profile/userinfos/userinfos.component.ts
+++ src/app/components/user/user-profile/userinfos/userinfos.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Client } from '../../../../core/models/Client';
+import { ClientService } from '../../../../core/services/client.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-userinfos',
@@ -22,5 +25,46 @@ export class UserinfosComponent {
   closeModal2() {
     this.isModalOpen2 = false;
   }
+
+
+
+  
+  clients: Client[] = [];
+
+ 
+
+  private clientSe : ClientService = inject(ClientService);
+  
+ 
+ listClient!:Observable<Client[]>
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class. 
+   this.index();
+   this.listClient=this.clientSe.getData;
+   
+  }
+    selecteID!:number;
+    selecteName!:Client
+    selecteProfession:any
+
+    trackClient(user:any,id:number){ 
+       this.selecteName=user
+       this.selecteID=id
+    }
+
+    index(){
+      this.clientSe.index()
+      this.clientSe.getData.subscribe({
+      next:(data)=>{
+        this.clients=data; 
+      },
+      error:(error:any)=>{
+        console.log(error);  
+      }
+      })
+
+    }
 
 }
