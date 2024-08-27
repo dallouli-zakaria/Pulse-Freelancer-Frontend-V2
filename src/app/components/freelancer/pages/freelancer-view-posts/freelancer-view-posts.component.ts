5732a7e6bd959b2bf78f494cce082cc550ac5815
zakaria <dalloulizakaria@gmail.com>
2024-08-27 12:10:47 +0100

    filter range

--- src/app/components/freelancer/pages/freelancer-view-posts/freelancer-view-posts.component.ts
+++ src/app/components/freelancer/pages/freelancer-view-posts/freelancer-view-posts.component.ts
@@ -6,6 +6,8 @@ import { PostsService } from '../../../../core/services/posts.service';
 import { Subscription, Subject } from 'rxjs';
 import { Offer } from '../../../../core/models/Offer';
 import { OffersService } from '../../../../core/services/offers.service';
+import { SkillService } from '../../../../core/services/skill.service';
+import { Skill } from '../../../../core/models/skill';
 
 @Component({
   selector: 'app-freelancer-view-posts',
@@ -18,13 +20,16 @@ export class FreelancerViewPostsComponent {
   freelancerid: number = this.authservice.parseID();
   company_name!: string;
   clientId!: number;
+  skills:Skill[]=[];
 
   constructor(
     private postsService: PostsService,
     private authservice: AuthService,
-    private clientservice: ClientService
+    private clientservice: ClientService,
+    private skillservice:SkillService
   ) {
     this.getposts();
+    this.loadSkills();
   }
 
   selectedPostId: number | null = null;
@@ -49,6 +54,14 @@ export class FreelancerViewPostsComponent {
     this.clientservice.getData$.subscribe((res: any) => {
       this.company_name = res.company_name;
       console.log(res);
+    });
+  }
+
+  //load all skills
+  loadSkills(): void {
+    this.skillservice.index();
+    this.skillservice.skillData.subscribe((res) => {
+      this.skills = res.sort((a, b) => a.title.localeCompare(b.title));
     });
   }
 }
