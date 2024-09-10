e7efc9705b60980df87e85030884377f37f87dc0
haidasse <j.haidasse@upm.ac.ma>
2024-09-10 16:32:26 +0100

    commited6

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files from the dist folder
+
 app.use(express.static(__dirname + '/dist/pulse-freelancer-frontend-v2'));
 
-// Catch all requests and send them to index.html
+
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + './public'+'/dist/pulse-freelancer-frontend-v2/browser/index.html'));  // Path to index.html
+  res.sendFile(path.join(__dirname +'/dist/pulse-freelancer-frontend-v2/browser/index.html')); 
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
