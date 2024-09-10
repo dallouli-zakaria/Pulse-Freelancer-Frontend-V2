1dcaff61cdee491fcfdb9e12ff3f14073440084c
haidasse <j.haidasse@upm.ac.ma>
2024-09-10 16:49:11 +0100

    commited8

--- server.js
+++ server.js
@@ -10,4 +10,4 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname +'/dist/pulse-freelancer-frontend-v2/browser/index.html')); 
 });
 
-// app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200);
