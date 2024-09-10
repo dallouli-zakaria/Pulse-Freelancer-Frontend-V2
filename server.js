0d5a701d854c54231eb4c7a04f8ca4dfcc457473
zakaria <dalloulizakaria@gmail.com>
2024-09-10 17:19:06 +0100

    heroku changes 7

--- server.js
+++ server.js
@@ -2,15 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files from the Angular app's 'dist' directory
-app.use(express.static('dist/pulse-freelancer-frontend-v2'));
+// Serve only the static files from the dist directory
+app.use(express.static(__dirname + '/dist'));
 
-// Redirect all requests to the Angular app's 'index.html'
-app.get('/*', function(req, res) {
-  res.sendFile('index.html',{root:'dist/pulse-freelancer-frontend-v2/browser/'});
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
-// Start the app by listening on the default Heroku port or port 8080
-app.listen(process.env.PORT || 8080, () => {
-  console.log('Server is running');
-});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
