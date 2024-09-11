598707dc007e00c0ad91e190bdc4af1027a09af4
haidasse <j.haidasse@upm.ac.ma>
2024-09-11 18:14:38 +0100

    deleted build12

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Move the app.listen statement before everything else
+app.listen(process.env.PORT || 8080);
 
+// Serve static files
 app.use(express.static(__dirname + '/dist/pulse-freelancer-frontend-v2'));
 
-
+// Handle all routes
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname +'/dist/pulse-freelancer-frontend-v2/browser/index.html')); 
+  res.sendFile(path.join(__dirname + '/dist/pulse-freelancer-frontend-v2/browser/index.html'));
 });
-
-app.listen(process.env.PORT || 8080);
